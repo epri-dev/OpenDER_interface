a6f0d7ced1244a0fbfdfc0afeff163ebd7f232ae
pyma001 <yma@epri.com>
2023-11-03 10:54:49 -0400

    adding copyright, update concept.png

--- src/opender_interface/voltage_regulator.py
+++ src/opender_interface/voltage_regulator.py
@@ -1,6 +1,16 @@
-# %%
+# Copyright © 2023 Electric Power Research Institute, Inc. All rights reserved.
 
-# -*- coding: utf-8 -*-
+# Redistribution and use in source and binary forms, with or without modification,
+# are permitted provided that the following conditions are met:
+# · Redistributions of source code must retain the above copyright notice,
+#   this list of conditions and the following disclaimer.
+# · Redistributions in binary form must reproduce the above copyright notice,
+#   this list of conditions and the following disclaimer in the documentation
+#   and/or other materials provided with the distribution.
+# · Neither the name of the EPRI nor the names of its contributors may be used
+#   to endorse or promote products derived from this software without specific
+#   prior written permission.
+
 """
 Created on Fri Feb  4 13:07:24 2022
 
